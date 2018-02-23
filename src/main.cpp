ca015d9c34f9fd2415b8aa6ee83940c2f5cfea5b
Student <student@ib.polsl.pl>
2018-02-23 14:08:27 +0100

    Commit trzeci i czwarty

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
-#include "trygonometria.h"
+#include "LaborkaConfig.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"
+#else
+	#include <cmath>
+#endif
 
 int  main() 
 { 
-	double angle1 = 30;
-
-	std::cout << degreemath::sin(angle1) << std::endl;
-	std::cout << degreemath::cos(angle1) << std::endl;
-	std::cout << degreemath::tg(angle1) << std::endl;
-	std::cout << degreemath::ctg(angle1) << std::endl;
+	double angle_deg = 30;
+	double angle_rad = (angle_deg * M_PI) /180;
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		std::cout << degreemath::sin(angle_deg) << std::endl;
+		std::cout << degreemath::cos(angle_deg) << std::endl;
+		std::cout << degreemath::tg(angle_deg) << std::endl;
+		std::cout << degreemath::ctg(angle_deg) << std::endl;
+	#else
+		std::cout << sin(angle_rad) << std::endl;
+		std::cout << cos(angle_rad) << std::endl;
+		std::cout << tan(angle_rad) << std::endl;
+		std::cout << 1/tan(angle_rad) << std::endl;
+	#endif
 
 	return 0; 
 }
