080faa8ddac96136f797688de8e6afcc989a2b1a
d3zzo <konradwyparlo@gmail.com>
2018-02-23 13:22:43 +0100

    Drugi commit

---
+++ src/trygonometria.cpp
@@ -0,0 +1,39 @@
+#define _USE_MATH_DEFINES
+#include "trygonometria.h"
+#include <cmath>
+
+double convertToRad(double degree)
+{
+	double rad = (degree * M_PI) /180;
+	return rad;
+}
+
+double degreemath::sin(double degree) {
+//zamiana stopni na radiany: pi radianów = 180 stopni
+//return std::sin(radian); //funkcja biblioteczna z piku <cmath>
+	double rad = convertToRad(degree);
+	return std::sin(rad);
+}
+
+double degreemath::cos(double degree) {
+//zamiana stopni na radiany: pi radianów = 180 stopni
+//return std::sin(radian); //funkcja biblioteczna z piku <cmath>
+	double rad = convertToRad(degree);
+	return std::sin(rad);
+}
+
+
+double degreemath::tg(double degree) {
+//zamiana stopni na radiany: pi radianów = 180 stopni
+//return std::sin(radian); //funkcja biblioteczna z piku <cmath>
+	double rad = convertToRad(degree);
+	return std::sin(rad);
+}
+
+
+double degreemath::ctg(double degree) {
+//zamiana stopni na radiany: pi radianów = 180 stopni
+//return std::sin(radian); //funkcja biblioteczna z piku <cmath>
+	double rad = convertToRad(degree);
+	return std::sin(rad);
+}
